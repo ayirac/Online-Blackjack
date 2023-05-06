e6ccc657114e7353cc2c260590fc6ea8c9e31cbd
Vell-ow <coviello_andrew@yahoo.com>
2023-05-06 13:49:03 -0700

    Created new InstructionsPanel.java

--- client/src/main/java/com/clientblackjack/gui/ActorCard.java
+++ client/src/main/java/com/clientblackjack/gui/ActorCard.java
@@ -5,6 +5,6 @@ import javax.swing.JLabel;
 public class ActorCard {
     private Deck deck_;
     // private Wager wager_; PlayerCard
-    private Avatar avatar_;
+    // private Avatar avatar_;
     private JLabel name_;
 }
