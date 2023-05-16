1d79578a7c08437fe4dfb73100c9eb6b08e6ae6b
ayirac <104404413+ayirac@users.noreply.github.com>
2023-05-15 18:19:26 -0700

    Delete main directory

--- main/java/com/clientblackjack/gui/DealerCard.java
+++
@@ -1,9 +0,0 @@
-package com.clientblackjack.gui;
-
-public class DealerCard extends ActorCard {
-    DealerCard() {
-        // able to customize dealer font/color, other attributes here in the future.
-        super("Dealer");
-    }
-
-}
