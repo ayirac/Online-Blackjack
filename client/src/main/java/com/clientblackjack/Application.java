fbb6c7b4ceaf0d4db4fb4c958a51e2a53fe7d68b
Andrew Coviello <78008163+Vell-ow@users.noreply.github.com>
2023-05-16 08:45:03 -0700

    Update Application.java
    Commented

--- client/src/main/java/com/clientblackjack/Application.java
+++ client/src/main/java/com/clientblackjack/Application.java
@@ -1,5 +1,5 @@
 package com.clientblackjack;
-
+// mainframe import from package gui
 import com.clientblackjack.gui.MainFrame;
 
 
@@ -7,9 +7,13 @@ public class Application
 {
     public static void main( String[] args )
     {
+        // establish mainframe from mainframe class which was imported
         MainFrame mainFrame = new MainFrame();
+        // swap main menu called on the mainframe object
         mainFrame.swapMainMenu();
+        // what is swap game? why is this line commented out
         //mainFrame.swapGame();
+        // main frame runs, sets up main menu for when opening obj (1280x720 is a standard full screen type)
         mainFrame.run(1280, 720);
     }
 }
