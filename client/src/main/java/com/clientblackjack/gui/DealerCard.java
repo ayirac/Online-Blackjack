0c4bfdf4f4895f3b6aa06713b27f84ac2f6b4f9c
Andrew Coviello <78008163+Vell-ow@users.noreply.github.com>
2023-05-17 14:26:49 -0700

    Update DealerCard.java
    Are you gonna add any more changes to this file?

--- client/src/main/java/com/clientblackjack/gui/DealerCard.java
+++ client/src/main/java/com/clientblackjack/gui/DealerCard.java
@@ -3,7 +3,7 @@ package com.clientblackjack.gui;
 public class DealerCard extends ActorCard {
     DealerCard() {
         // able to customize dealer font/color, other attributes here in the future.
-        super("Dealer");
+        super("Dealer"); // are you gonna implement any more functions here?
     }
 
 }
