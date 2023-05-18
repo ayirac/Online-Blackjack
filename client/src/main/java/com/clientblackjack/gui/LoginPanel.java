2ecf8b2cec31163e88287b4f51ba706f32f9dbd6
Andrew Coviello <78008163+Vell-ow@users.noreply.github.com>
2023-05-18 12:26:13 -0700

    Update LoginPanel.java
    Commented Login Panel

--- client/src/main/java/com/clientblackjack/gui/LoginPanel.java
+++ client/src/main/java/com/clientblackjack/gui/LoginPanel.java
@@ -10,103 +10,101 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import java.awt.*;
 
-public class LoginPanel {
-    public static final String SUBMIT = "submit";
-    public static final String[] BUTTONS = {SUBMIT};
-    JButton backButton = new JButton("Back");
-    private JPanel panel = new JPanel(new GridBagLayout());
-    GridBagConstraints constraints = new GridBagConstraints();
+public class LoginPanel { // login panel class
+    public static final String SUBMIT = "submit"; // submit string (immutable)
+    public static final String[] BUTTONS = {SUBMIT}; // submit button (immutable)
+    JButton backButton = new JButton("Back"); // back button
+    private JPanel panel = new JPanel(new GridBagLayout()); // new JPanel with grid bag layout
+    GridBagConstraints constraints = new GridBagConstraints(); // new constraints with grid bag constraints
 
-    public JLabel loginLabel = new JLabel("Login");
+    public JLabel loginLabel = new JLabel("Login"); // new label "Login" JLabel
 
-    public JLabel usernameLabel = new JLabel("Username");
-    public JLabel passwordLabel = new JLabel("Password");
-    public JTextField usernameField = new JTextField();
-    public JPasswordField passwordField = new JPasswordField();
-    public JButton loginBtn = new JButton("Submit");
+    public JLabel usernameLabel = new JLabel("Username"); // new label "Username" label
+    public JLabel passwordLabel = new JLabel("Password"); // new label "Password" label
+    public JTextField usernameField = new JTextField(); // new JTextField "usernameField" field
+    public JPasswordField passwordField = new JPasswordField(); // new JPasswordField "passwordField" field
+    public JButton loginBtn = new JButton("Submit"); // new JButton "Submit" button
 
 
-    public LoginPanel(ActionListener listener) {
+    public LoginPanel(ActionListener listener) { // login panel with listener as param
 
-        Font font = loginLabel.getFont();
+        Font font = loginLabel.getFont(); // login label font is the value of the new font object
         Font newFont = font.deriveFont(font.getSize() + 7f); // Increase the size by 5 points
-        loginLabel.setFont(newFont);
-        loginLabel.setForeground(Color.WHITE);
-        usernameLabel.setForeground(Color.WHITE);
-        passwordLabel.setForeground(Color.WHITE);
-        loginBtn.setName(SUBMIT);
-        backButton.setName("back");
+        loginLabel.setFont(newFont); // set font for login label to new font
+        loginLabel.setForeground(Color.WHITE); // set white foreground for login label
+        usernameLabel.setForeground(Color.WHITE); // set white foreground for username label
+        passwordLabel.setForeground(Color.WHITE); // set white foreground for password label
+        loginBtn.setName(SUBMIT); // set login button name to SUBMIT
+        backButton.setName("back"); // set back button name to back
 
-        panel = new JPanel() {
+        panel = new JPanel() { // new jPanel called panel
             @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
+            protected void paintComponent(Graphics g) { // paint component with graphics g param is a protected method that controls how the panel looks
+                super.paintComponent(g); // call super recursively
                 // gets image, already resized to 1280 x 720
-                ImageIcon backgroundImage = new ImageIcon(getClass().getResource("/images/bg_2.jpg"));
+                ImageIcon backgroundImage = new ImageIcon(getClass().getResource("/images/bg_2.jpg")); // call predefined, saved bg_2 image
                 // displays the background image
-                g.drawImage(backgroundImage.getImage(), 0, 0, getWidth(), getHeight(), this);
+                g.drawImage(backgroundImage.getImage(), 0, 0, getWidth(), getHeight(), this); // draw image with given image, no offset, width and height params & this
             }
         };
 
 
 
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.insets = new Insets(5, 5, 5, 5);
+        constraints.fill = GridBagConstraints.HORIZONTAL; // constraints fill using the horizontal preset
+        constraints.insets = new Insets(5, 5, 5, 5); // insets are a perfect square of 5s
         // how big the username and password fields will be
-        passwordField.setPreferredSize(new Dimension(200, 20));
-        usernameField.setPreferredSize(new Dimension(200, 20));
+        passwordField.setPreferredSize(new Dimension(200, 20)); // 200 wide, 20 high
+        usernameField.setPreferredSize(new Dimension(200, 20)); // 200 wide, 20 high
         // adding login label to the login panel
-        loginLabel.setHorizontalAlignment(JLabel.CENTER);
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.gridwidth = 2;
-        panel.add(loginLabel, constraints);
+        loginLabel.setHorizontalAlignment(JLabel.CENTER); // set horizontal alignment to be centered
+        constraints.gridx = 0; // no horizontal skew
+        constraints.gridy = 0; // no vertical skew
+        constraints.gridwidth = 2; // width of 2 units
+        panel.add(loginLabel, constraints); // add login label with given constraints to the panel
         // adding username label to the login panel
-        constraints.gridx = 0;
-        constraints.gridy = 1;
-        constraints.gridwidth = 2;
-        panel.add(usernameLabel, constraints);
+        constraints.gridx = 0; // no horizontal skew
+        constraints.gridy = 1; // 1 unit down
+        constraints.gridwidth = 2; // width of 2 units
+        panel.add(usernameLabel, constraints); // add username label with given constraints to the panel
         // adding username field to the login panel
-        constraints.gridx = 0;
-        constraints.gridy = 2;
-        panel.add(usernameField, constraints);
+        constraints.gridx = 0; // no horizontal skew
+        constraints.gridy = 2; // 2 units down
+        panel.add(usernameField, constraints); // add username field with given constraints to the panel
         // adding password label to the login panel
-        constraints.gridx = 0;
-        constraints.gridy = 3;
-        panel.add(passwordLabel, constraints);
+        constraints.gridx = 0; // no horizontal skew
+        constraints.gridy = 3; // 3 units down
+        panel.add(passwordLabel, constraints); // add password label with given constraints to the panel
         // adding password field to the login panel
-        constraints.gridx = 0;
-        constraints.gridy = 4;
-        panel.add(passwordField, constraints);
+        constraints.gridx = 0; // no horizontal skew
+        constraints.gridy = 4; // 4 units down
+        panel.add(passwordField, constraints); // add password field with given constraints to the panel
         // adding Login button to the login panel
-        constraints.gridx = 0;
-        constraints.gridy = 5;
-        constraints.gridwidth = 2;
-        panel.add(loginBtn, constraints);
+        constraints.gridx = 0; // no horizontal skew
+        constraints.gridy = 5; // 5 units down
+        constraints.gridwidth = 2; // width of 2 units
+        panel.add(loginBtn, constraints); // add login button with given constraints to the panel
 
         //adding back button.
-        constraints.gridx = 0;
-        constraints.gridy = 6;
-        constraints.gridwidth = 2;
-        panel.add(backButton, constraints);
-
-
+        constraints.gridx = 0; // no horizontal skew
+        constraints.gridy = 6; // 6 units down
+        constraints.gridwidth = 2; // width of 2 units
+        panel.add(backButton, constraints); // add back button with given constraints to the panel
 
         // enabling the login button to accept clicks
-        loginBtn.addActionListener(listener);
-        backButton.addActionListener(listener);
+        loginBtn.addActionListener(listener); // add action listener to login button
+        backButton.addActionListener(listener); // add action listener to back button
 
     }
 
-    public JPanel getPanel() {
+    public JPanel getPanel() { // just returns the panel we made
         return this.panel;
     }
 
-    public String getUsername() {
+    public String getUsername() { // returns username entered
         return this.usernameField.getText(); 
     }
 
-    public String getPasswordHash() {
+    public String getPasswordHash() { // returns password hash using a new string that just gets the password
         return new String(this.passwordField.getPassword());
     }
 }
